6b23f9224d27e0e7de2ee2669f74a9d915292e66
tuwafula <wafulaat@gmail.com>
2024-01-11 22:48:35 +0300

    modified the setup file

--- setup.sh
+++ setup.sh
@@ -17,6 +17,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 # Check if the superuser already exists
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(email='admin@gmail.com').exists():
     User.objects.create_superuser('admin@gmail.com', 'wafulaat@gmail.com', 'admin1234')
 EOF
