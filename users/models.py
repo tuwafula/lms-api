3f8ee923ea728fff12530f7accaf64a2c66add91
tuwafula <wafulaat@gmail.com>
2024-01-13 15:48:18 +0300

    modified models.py

--- users/models.py
+++ users/models.py
@@ -13,6 +13,7 @@ class CustomUserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
